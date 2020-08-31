9090e79d2e28268361afc6f412658bad52c9d16e
Zemian Deng <zemian.deng@oracle.com>
2020-08-31 09:24:18 -0400

    Add notes on function tests

--- php-scripts/function-redefined-error.php
+++ php-scripts/function-redefined-error.php
@@ -1,12 +1,11 @@
 <?php
-
-// TODO: Why this produce silent error??
 // Function overloading does not work
 // PHP does not support function overloading, nor is it possible to undefine or redefine previously-declared functions.
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL & E_STRICT);
+//
+// NOTE: This produce silent error in PHP 5.6, but PHP 7 will give you a nice "Fatal error: Cannot redeclare foo()"
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL & E_STRICT);
 
 function foo() {
     echo "first time";
