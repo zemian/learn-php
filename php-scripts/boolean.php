27472b5f21c0e9df732fa9f36f449f064590a84c
Zemian Deng <zemian.deng@oracle.com>
2020-09-02 12:54:52 -0400

    Add comparison boolean notes

--- php-scripts/boolean.php
+++ php-scripts/boolean.php
@@ -34,4 +34,26 @@ var_dump((bool) array(12)); // bool(true)
 var_dump((bool) array());   // bool(false)
 var_dump((bool) "false");   // bool(true)
 
+// Comparison
+// ==========
+
+// https://www.php.net/manual/en/language.operators.comparison.php
+
+// $a == $b 	Equal 	TRUE if $a is equal to $b after type juggling.
+// $a === $b 	Identical 	TRUE if $a is equal to $
+// $a <=> $b 	Spaceship 	An integer less than, equal to, or greater than zero when $a is less than, equal to, or greater than $b, respectively. Available as of PHP 7. 
+
+// If you compare a number with a string or the comparison involves numerical strings, then each string is converted to a number and the comparison performed numerically. 
+var_dump(0 == "a"); // 0 == 0 -> true
+var_dump("1" == "01"); // 1 == 1 -> true
+var_dump("10" == "1e1"); // 10 == 10 -> true
+var_dump(100 == "1e2"); // 100 == 100 -> true
+
+// Bool and null are compared as bool always
+var_dump(1 == TRUE);  // TRUE - same as (bool)1 == TRUE
+var_dump(0 == FALSE); // TRUE - same as (bool)0 == FALSE
+var_dump(100 < TRUE); // FALSE - same as (bool)100 < TRUE
+var_dump(-10 < FALSE);// FALSE - same as (bool)-10 < FALSE
+var_dump(min(-100, -10, NULL, 10, 100)); // NULL - (bool)NULL < (bool)-100 is FALSE < TRUE
+
 ?>
