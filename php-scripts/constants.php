75e5bd6003437a8bc60ec18beb13e6ed02a45ff0
Zemian Deng <zemian.deng@oracle.com>
2020-10-16 13:23:55 -0400

    Update vars tests

---
+++ php-scripts/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+//// Constants
+//define("FOO", "foo");
+//print_r(FOO);
+
+// Magic Constants - https://www.php.net/manual/en/language.constants.predefined.php
+echo __LINE__, "\n";
+echo __FILE__, "\n";
+echo __DIR__, "\n";
