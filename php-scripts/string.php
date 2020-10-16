e62c9bbe80e58ff879abe387ac8accdc6191dd54
Zemian Deng <zemian.deng@oracle.com>
2020-10-16 14:50:10 -0400

    string tests

--- php-scripts/string.php
+++ php-scripts/string.php
@@ -1,92 +1,105 @@
 <?php
 
-// PHP_EOL = "\n";
-echo "Hi\n";
-echo "Hi".PHP_EOL;
+//// Single quote - string literal
+//// Double quote - string evaluation (variable expansion) NOTE: only variable, it will not evaluate expression or function call!
+////echo 'this is a simple string';
+//$foo = 'bar';
+//echo "This is $foo";
 
-echo 'this is a simple string';
-
-echo 'You can also have embedded newlines in
-strings this way as it is
-okay to do';
-
-// Outputs: Arnold once said: "I'll be back"
-echo 'Arnold once said: "I\'ll be back"';
-
-// Outputs: You deleted C:\*.*?
-echo 'You deleted C:\\*.*?';
-
-// Outputs: You deleted C:\*.*?
-echo 'You deleted C:\*.*?';
-
-// Outputs: This will not expand: \n a newline
-echo 'This will not expand: \n a newline';
-
-// Outputs: Variables do not $expand $either
-echo 'Variables do not $expand $either';
+//// New line constant - OS dependent
+//// PHP_EOL = "\n";
+//echo "Hi\n";
+//echo "Hi".PHP_EOL;
 
 
-// Dollar variables
-$juice = "apple";
+//// String literal can span multiple lines
+//echo 'You can also have embedded newlines in
+//strings this way as it is
+//okay to do';
 
-echo "He drank some $juice juice.".PHP_EOL;
-// Invalid. "s" is a valid character for a variable name, but the variable is $juice.
-//echo "He drank some juice made of $juices.";
-// Valid. Explicitly specify the end of the variable name by enclosing it in braces:
-echo "He drank some juice made of ${juice}s.";
+//// Single quote escape and avoid variable expand
+//echo 'Arnold once said: "I\'ll be back"';
+//echo 'You deleted C:\\*.*?';
+//echo 'You deleted C:\*.*?';
+//echo 'This will not expand: \n a newline';
+//echo 'Variables do not $expand $either';
 
-// Accessing characters in string
-echo PHP_EOL;
-$s = "Hello";
-echo '$s[1]=', $s[1], "\n";
-echo 'strlen($s)=', strlen($s), "\n";
-for ($i = 0; $i < strlen($s); $i++) {
-    echo "\$s[$i]=", $s[$i], "\n";
-}
+//// Double quote strings
+//$foo = "bar";
+//$a = [1, 2, 3];
+//echo "This will expand into new line\n";
+//echo "Hello \$foo\n";
+//echo "Hello $foo\n";
+//echo "Hello ${foo}\n"; // not proper but works - see more below
+//echo "Hello {$foo}\n"; // a more proper way
+//echo "Hello $a[0]\n";
+//echo "Hello ${a[0]}\n";
+////echo "Expression ${$a[0] + $a[1]}\n"; // error - you cannot evaluate expression within the ${}
+//echo "Expression " . ($a[0] + $a[1]) . "\n"; // do this instead
 
-// Heredoc
-$str = <<<EOD
-Example of string
-spanning multiple lines
-using heredoc syntax.
-EOD;
-var_dump($str);
-
-// Split string
-$array = explode("/", "foo/bar/baz");
-var_dump($array);
+//// More on Double string expression
+//$x = 1; $y = 2;
+//function get_name() { return "x"; }
+//echo "Expression {$x}\n";
+////echo "Expression {$x + $y}\n"; // error
+//echo "Expression {${get_name()}}\n"; // NOTE: The get_name() must return a variable name!
+//echo "Expression {{get_name()}}\n"; // Will not evaluated
+//echo "Expression {get_name()}\n"; // Will not evaluated
+////echo "Expression {$get_name()}\n"; // error
 
 
-print "This spans
-multiple lines. The newlines will be
-output as well";
+//echo "Expression {$x + $y}\n"; // error
 
-print "This spans\nmultiple lines. The newlines will be\noutput as well.";
+// Single vs Double quote speed.
+// The double quote string needs to expand variable, so technically slower, but for all practical purpose
+// developers do not need to worry about it.
 
-print "escaping characters is done \"Like this\".";
+//// HEREDOC string
+//$name = "test";
+//$str = <<<EOD
+//Example $name of string
+//spanning multiple lines
+//using heredoc syntax.
+//EOD;
+//echo $str;
 
-// You can use variables inside a print statement
-$foo = "foobar";
-$bar = "barbaz";
+////  BAD - EOD; should not have space in front.
+//echo <<<EOD
+//test
+//    EOD;
 
-print "foo is $foo"; // foo is foobar
+////  This is okay:
+//echo <<<EOD
+//test
+//EOD;
 
-// You can also use arrays
-$bar = array("value" => "foo");
+////  But this is okay too?
+//echo <<<EOD
+//    test
+//    two
+//    EOD;
 
-print "this is {$bar['value']} !"; // this is foo !
+//// NOWDOC string
+//// The HEREDOC is like double quote string, while NOWDOC is like single quote string.
+//// Notice you single quote the 'EOD'
+//echo <<<'EOD'
+//Hello world\n
+//EOD;
 
-// Using single quotes will print the variable name, not the value
-print 'foo is $foo'; // foo is $foo
+//// Accessing characters in string
+//echo PHP_EOL;
+//$s = "Hello";
+//echo '$s[1]=', $s[1], "\n";
+//echo 'strlen($s)=', strlen($s), "\n";
+//for ($i = 0; $i < strlen($s); $i++) {
+//    echo "\$s[$i]=", $s[$i], "\n";
+//}
 
-// If you are not using any other characters, you can just print variables
-print $foo;          // foobar
+// Split and Joining string
+// NOTE: Do not use str_split() - it is used to split string in equal chunks!
+//print_r(explode("/", "foo/bar/baz"));
+//print_r(implode(":", range(1, 5)));
 
-$variable = "'I am a variable'";
-
-print <<<END
-This uses the "here document" syntax to output
-multiple lines with $variable interpolation. Note
-that the here document terminator must appear on a
-line with just a semicolon no extra whitespace!
-END;
+// Substring
+$s = "The quick brown fox jumps over the lazy dog.";
+//echo "len=${strlen($s)}";
