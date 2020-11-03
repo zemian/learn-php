293da3a2e8edab568174a5386cb5de55d4a51c0e
Zemian Deng <zemiandeng@gmail.com>
2020-11-03 17:09:07 -0500

    Add system info to admin home page

--- php-web/login-app/admin/index.php
+++ php-web/login-app/admin/index.php
@@ -4,13 +4,53 @@ require_once '../app.php';
 if (!$app->isUserLoggedIn()) {
     $app->redirect('/login-app/login.php');
 }
+
+$php_version = phpversion();
+$mysql_version = $app->db->query("SELECT VERSION()")->fetch()[0];
+$server = $_SERVER['SERVER_SOFTWARE'];
+$os = PHP_OS;
 ?>
 
 <?php $app->header('admin'); ?>
 
-<section class="section">
-    <h1>You are in admin space!</h1>
-</section>
+<div class="hero">
+    <div class="hero-body">
+        <div class="container">
+            <div class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">PHP</p>
+                        <p class="title"><?php echo $php_version; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">MySQL</p>
+                        <p class="title"><?php echo $mysql_version; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">Server</p>
+                        <p class="title"><?php echo $server; ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="level">
+                <div class="level-item has-text-centered">
+                    <div>
+                        <p class="heading">OS</p>
+                        <p class="title"><?php echo $os; ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php $app->footer(); ?>
 
