33fccc0669da49dfa34ae61ddbb573ad4e47f1a5
Zemian Deng <zemiandeng@gmail.com>
2021-03-05 14:00:59 -0500

    Add api/notes-api sample

--- api/index.php
+++ api/index.php
@@ -1,25 +1,14 @@
 <?php
-header('Content-Type: application/json');
-switch($_GET['action'] ?? '') {
-    case 'server':
-        echo json_encode($_SERVER);
-        break;
-    case 'request':
-        echo json_encode($_REQUEST);
-        break;
-    case 'cookie':
-        echo json_encode($_COOKIE);
-        break;
-    case 'env':
-        // Note that $_ENV might be empty and requires php config enabled. Use "getenv()" instead.
-        echo json_encode(getenv());
-        break;
-    case 'locales':
-        echo json_encode(ResourceBundle::getLocales(''));
-        break;
-    default:
-        echo json_encode(array(
-            'queryParametersUsage' => '?action=server|request|cookie|env|locales',
-            'timestamp' => date('c')
-        ));
-}
+$output = array(
+    'timestamp' => date('c'),
+    'resource' => $_SERVER['PATH_INFO'],
+    'cwd' => getcwd(),
+    'cwd_pathinfo' => pathinfo(getcwd()),
+    'server' => $_SERVER,
+    'request' => $_REQUEST,
+    'cookie' => $_COOKIE,
+    'env' => getenv(),
+    'locales' => json_encode(ResourceBundle::getLocales('')),
+);
+header('content-type: application/json');
+echo json_encode($output);
