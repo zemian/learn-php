6a6d433479d03c0e6f172df60c156c29f1046295
Zemian Deng <zemiandeng@gmail.com>
2021-03-04 19:18:04 -0500

    Fix options-api

--- options-api/index.php
+++ options-api/index.php
@@ -3,41 +3,7 @@ $env = parse_ini_file("../env.ini", true);
 $pdocfg = $env['pdo'];
 $db = new PDO($pdocfg['dsn'], $pdocfg['username'], $pdocfg['password']);
 
-function api_create_table() {
-    global $db;
-    $sql = <<< HERE
-        CREATE TABLE IF NOT EXISTS options (
-            id INT PRIMARY KEY AUTO_INCREMENT,
-            create_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            update_ts DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            name VARCHAR(500) NOT NULL UNIQUE,
-            value TEXT NOT NULL,
-            comment TEXT NULL
-        )
-HERE;
-    $stmt = $db->prepare($sql);
-    if ($stmt->execute()) {
-        return array("status" => "Table is created.");
-    } else {
-        return array("status" => "Create failed.", "error" => $db->errorInfo());
-    }
-}
-
-function api_sample() {
-    global $db;
-    $stmt = $db->prepare('INSERT INTO options (name, value) VALUE (?, ?)');
-    $count = 0;
-    foreach ($_SERVER as $k => $v) {
-        $stmt->bindParam(1, $k);
-        $stmt->bindParam(2, $v);
-        if ($stmt->execute()) {
-            $count++;
-        }
-    }
-    return array("status" => "Sample inserted.", "count" => $count);
-}
-
-function api_list() {
+function rest_options_list() {
     global $db;
     $offset = $_GET['offset'] ?? 0;
     $limit = min($_GET['limit'] ?? 25, 500);
@@ -52,7 +18,7 @@ function api_list() {
     return array("items" => $rows);
 }
 
-function api_get($name) {
+function rest_options_get($name) {
     global $db;
     if (!$name) {
         return array("error" => "You need 'name' parameter.");
@@ -68,20 +34,20 @@ function api_get($name) {
     return array("error" => "There is no record match to 'name'.");
 }
 
-function api_delete($name) {
+function rest_options_delete($opt) {
     global $db;
-    if (!$name) {
+    if (!$opt->name) {
         return array("error" => "You need 'name' parameter.");
     }
     $stmt = $db->prepare('DELETE FROM options WHERE name = ?');
-    $stmt->bindParam(1, $name);
+    $stmt->bindParam(1, $opt->name);
     if ($stmt->execute()) {
         return array("status" => "Deleted successfully.");
     }
     return array("error" => "There is no record match to 'name'.");
 }
 
-function api_update($opt) {
+function rest_options_update($opt) {
     global $db;
     $stmt = $db->prepare('UPDATE options SET value = ? WHERE name = ?');
     $stmt->bindParam(1, $opt->value);
@@ -92,42 +58,39 @@ function api_update($opt) {
     return array("error" => "Unable to update record.");
 }
 
-function api_create($opt) {
+function rest_options_create($opt) {
     global $db;
-    $stmt = $db->prepare('INSERT INTO options (name, value) VALUE (?, ?)');
+    $stmt = $db->prepare('INSERT INTO options (name, value, comment) VALUE (?, ?, ?)');
     $stmt->bindParam(1, $opt->name);
     $stmt->bindParam(2, $opt->value);
+    $stmt->bindParam(3, $opt->comment);
     if ($stmt->execute()) {
         return array("status" => "Created successfully.");
     }
     return array("error" => "Unable to create record.");
 }
 
-//header('Content-Type: application/json');
-var_dump($_SERVER['REQUEST_METHOD']);
-//if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-//    echo json_encode(api_delete($_GET['name'] ?? null));
-//} else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-//    $body = file_get_contents('php://input');
-//    $opt = json_decode($body);
-//    echo json_encode(api_update($opt));
-//} else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-//    $body = file_get_contents('php://input');
-//    $opt = json_decode($body);
-//    echo json_encode(api_create($opt));
-//} else {
-//    switch($_GET['resources'] ?? '') {
-//        case 'create_table':
-//            echo json_encode(api_create_table());
-//            break;
-//        case 'sample':
-//            echo json_encode(api_sample());
-//            break;
-//        default:
-//            if (isset($_GET['name'])) {
-//                echo json_encode(api_get($_GET['name']));
-//            } else {
-//                echo json_encode(api_list());
-//            }
-//    }
-//}
+header('Content-Type: application/json');
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $body = file_get_contents('php://input');
+        $opt = json_decode($body);
+        echo json_encode(rest_options_create($opt));
+        break;
+    case 'PUT':
+        $body = file_get_contents('php://input');
+        $opt = json_decode($body);
+        echo json_encode(rest_options_update($opt));
+        break;
+    case 'DELETE':
+        $body = file_get_contents('php://input');
+        $opt = json_decode($body);
+        echo json_encode(rest_options_delete($opt));
+        break;
+    default:
+        if (isset($_GET['name'])) {
+            echo json_encode(rest_options_get($_GET['name']));
+        } else {
+            echo json_encode(rest_options_list());
+        }
+}
