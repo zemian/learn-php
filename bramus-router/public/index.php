8ce338a4681b8daa22db47bd91137b8a916cd9ab
Zemian Deng <zemiandeng@gmail.com>
2021-03-28 17:52:27 -0400

    Move index.php into public

--- bramus-router/public/index.php
+++ bramus-router/public/index.php
@@ -3,7 +3,7 @@
 // composer require bramus/router
 
 // Require composer autoloader
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 
 // Create Router instance
 $router = new \Bramus\Router\Router();
