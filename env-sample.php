82e8a21815bc9e8610fa60162ba21b7c9385666c
Zemian Deng <zemian.deng@oracle.com>
2021-04-18 11:39:22 -0400

    Update env-sample.php

--- env-sample.php
+++ env-sample.php
@@ -1,5 +1,8 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'mydb');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+/* Application Absolute Path */
+define('APP_PATH', __DIR__);
+
+/* Database Connection Settings */
+define('DB_DSN', 'mysql:host=localhost;dbname=mydb');
+define('DB_USER', 'zemian');
+define('DB_PASSWORD', 'test123');
