3ff18bb3621b649dae23736a6b2f73902a646eec
Zemian Deng <zemiandeng@gmail.com>
2021-04-30 10:57:08 -0400

    Improved env.php

--- env.php
+++ env.php
@@ -1,6 +1,11 @@
 <?php
 /**
- * Parse .env file and return array of env key value pairs.
+ * Parse .env file and return array of env key value pairs. It supports the following:
+ * - Use "KEY = VALUE" per each line. Space is ignored.
+ * - Use "#" for line comment line. Or comment may be used at the end of line too (after VALUE).
+ * - Both empty and comment lines are skipped.
+ * - Empty VALUE will be stored as empty string ('') in array.
+ * - Single or double quotes around VALUE will be stripped. But not within the VALUE.
  */
 function parse_dot_env(string $file = '.env'): array {
     $handle = fopen($file, 'r');
@@ -15,8 +20,12 @@ function parse_dot_env(string $file = '.env'): array {
 
             // Skip comment lines, or remove comment portion
             $stripped_line = strstr($line, '#', true);
-            if ($stripped_line !== false && $stripped_line === '') {
-                continue;
+            if ($stripped_line !== false) {
+                if ($stripped_line === '') {
+                    continue;
+                } else {
+                    $line = $stripped_line; // Removed comment at end of line, still has data
+                }
             }
 
             // Parse key = value pair
