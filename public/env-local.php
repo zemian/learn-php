a843d39938bb22f72f5906cf5abb904e7a967d08
Zemian Deng <zemiandeng@gmail.com>
2021-04-30 09:58:00 -0400

    Add .env file

---
+++ public/env-local.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Provide local environment variables
+ */
+
+/* Application Absolute Path */
+define('APP_PATH', __DIR__);
+
+/* Database Connection Settings */
+define('DB_DSN', 'mysql:host=localhost;dbname=mydb');
+define('DB_USER', 'zemian');
+define('DB_PASSWORD', 'test123');
