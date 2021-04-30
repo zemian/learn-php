e542c9d95b6e722c10bdc1cf2d570ac4cd3ca725
Zemian Deng <zemiandeng@gmail.com>
2021-04-30 10:46:24 -0400

    Add env.php parser

--- bin/env.php
+++ bin/env.php
@@ -1,9 +1,47 @@
 <?php
-/*
- * Provide default environment variables if env-local.php is not given.
+/**
+ * Parse .env file and return array of env key value pairs.
  */
-if (file_exists('env-local.php'))
-	require_once 'env-local.php';
+function parse_dot_env(string $file = '.env'): array {
+    $handle = fopen($file, 'r');
+    if ($handle) {
+        $env = array();
+        while (($line = fgets($handle)) !== false) {
+            // Skip empty lines
+            $line = trim($line);
+            if (!$line) {
+                continue;
+            }
 
-if (!defined('DATA_DIR'))
-	define('DATA_DIR', __DIR__);
+            // Skip comment lines, or remove comment portion
+            $stripped_line = strstr($line, '#', true);
+            if ($stripped_line !== false && $stripped_line === '') {
+                continue;
+            }
+
+            // Parse key = value pair
+            // Empty value will be stored as empty string ('').
+            $pair = preg_split('/\s*=\s*/', $line, 2);
+            if (count($pair) === 2) {
+                $value = $pair[1];
+
+                // Remove quotes from value
+                $value = rtrim($value, '"');
+                $value = ltrim($value, '"');
+                $value = rtrim($value, "'");
+                $value = ltrim($value, "'");
+
+                $env[$pair[0]] = $value;
+            } else if (count($pair) === 1) {
+                $env[$pair[0]] = '';
+            }
+        }
+        fclose($handle);
+        return $env;
+    } else {
+        throw new Exception("File not found: $file");
+    }
+}
+
+// Test
+//print_r(parse_dot_env());
