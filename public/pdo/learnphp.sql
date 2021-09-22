d9bfa11a9208324354c9f0eea6836ba614f406ae
Zemian Deng <zemiandeng@gmail.com>
2021-09-22 12:42:10 -0400

    Update db default value notes

--- public/pdo/learnphp.sql
+++ public/pdo/learnphp.sql
@@ -6,7 +6,7 @@ select * from category where parent_id is not null;
 drop table if exists datatype;
 create table datatype (
     id int primary key auto_increment,
-    created_dt timestamp default current_timestamp,
+    created_dt timestamp default CURRENT_TIMESTAMP,
     modified_dt datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     name varchar(100),
     comment text,
@@ -31,16 +31,19 @@ insert into datatype(name, modified_dt) values('CURRENT_TIMESTAMP()', CURRENT_TI
 insert into datatype(name, modified_dt) values('ds string', '2021-12-31 08:00:01');
 
 -- default value
--- What happen if user manually update "modified_dt" ?
+-- What happen if user manually update "modified_dt" ? - User wins.
+/* start_time time DEFAULT (CURRENT_TIME()), /* For MySQL 8.0 we can call function, but not before. */
+/* NOTE: TEXT can not have default value */
+/* TODO: How can we default to use CURRENT_DATE() and CURRENT_TIME() ? */
 create table defvalue (
     id int primary key auto_increment,
-    created_dt timestamp default current_timestamp,
+    created_dt timestamp DEFAULT CURRENT_TIMESTAMP,
     modified_dt datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    name varchar(100) default 'test-name',
-    comment text default 'test-comment',
+    name varchar(100) DEFAULT 'test-name',
+    comment text,
     birth_date date,
     start_time time,
-    count int,
-    point double,
-    amount decimal(12, 2)
+    count int DEFAULT -1,
+    point double DEFAULT 0.0,
+    amount decimal(12, 2) DEFAULT 0.0
 );
