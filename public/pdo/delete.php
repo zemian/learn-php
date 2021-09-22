c55e97f2f76333b3f51e49298fe7fbb93f4b3ce0
Zemian Deng <zemiandeng@gmail.com>
2021-09-22 08:27:00 -0400

    Add update demo

--- public/pdo/delete.php
+++ public/pdo/delete.php
@@ -7,22 +7,24 @@ $pdo = new PDO(DB_DSN, DB_USER, DB_PASSWORD);
 // Delete by id
 // NOTE: Even if the ID does not exist in DB, the result will return 1 for success!
 $stmt = $pdo->prepare('DELETE FROM category where id = ?');
-$stmt->bindValue(1, 1, PDO::PARAM_INT);
+$id = 1;
+$stmt->bindValue(1, $id, PDO::PARAM_INT);
 $result = $stmt->execute();
 if ($result === false) {
     $error = $stmt->errorInfo();
 } else {
-    $data[] = ['id' => 1, 'result' => $result, 'deleted_count' => $stmt->rowCount()];
+    $data[] = ['id' => $id, 'result' => $result, 'deleted_count' => $stmt->rowCount()];
 }
 
 // Delete by id - loosely
 // NOTE: The parameter can be either int or string!
 $stmt = $pdo->prepare('DELETE FROM category where id = ?');
-$result = $stmt->execute(['2']);
+$id = '2';
+$result = $stmt->execute([$id]);
 if ($result === false) {
     $error = $stmt->errorInfo();
 } else {
-    $data[] = ['id' => '2', 'result' => $result, 'deleted_count' => $stmt->rowCount()];
+    $data[] = ['id' => $id, 'result' => $result, 'deleted_count' => $stmt->rowCount()];
 }
 ?>
 <!doctype html>
