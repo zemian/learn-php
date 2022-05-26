9d2eb8856b362f0d6b1422a154ca7b13f77be4ba
Zemian Deng <zemiandeng@gmail.com>
2022-05-26 11:15:46 -0400

    Turns filter list into a select control

--- webroot/form/filters.php
+++ webroot/form/filters.php
@@ -1,5 +1,6 @@
-<?php
-// PHP built in filters
-foreach(filter_list() as $filter) {
-    echo "<p>$filter</p>";
-}
+<lable>PHP Filters</lable>
+<select name="filter">
+    <?php foreach(filter_list() as $filter): ?>
+    <option value="<?php echo $filter?>"><?php echo $filter?></option>
+    <?php endforeach; ?>
+</select>
