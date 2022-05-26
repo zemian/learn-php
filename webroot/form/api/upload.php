7a796fe5735c62d952fd29d8eebee5f27bfb4233
Zemian Deng <zemiandeng@gmail.com>
2022-05-26 12:30:54 -0400

    Add curl examples

--- webroot/form/api/upload.php
+++ webroot/form/api/upload.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * A simple API to accept file upload.
+ *
+ * How to test this API with CURL:
+ *   curl http://localhost:3000/form/api/upload.php
+ *   curl -F 'data=@/Users/zemian/Desktop/test.png' http://localhost:3000/form/api/upload.php
+ */
+
 header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST");
