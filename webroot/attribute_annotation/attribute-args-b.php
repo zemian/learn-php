05a2348e7f18a86951d14c625099b89c4d630984
Zemian Deng <zemiandeng@gmail.com>
2022-05-28 15:41:13 -0400

    Add PHP attribute demos

---
+++ webroot/attribute_annotation/attribute-args-b.php
@@ -0,0 +1,22 @@
+<?php
+//https://www.php.net/manual/en/language.attributes.syntax.php
+
+namespace Another;
+
+use MyExample\MyAttribute;
+
+#[MyAttribute]
+#[\MyExample\MyAttribute]
+#[MyAttribute(1234)]
+#[MyAttribute(value: 1234)]
+#[MyAttribute(MyAttribute::VALUE)]
+#[MyAttribute(array("key" => "value"))]
+#[MyAttribute(100 + 200)]
+class Thing
+{
+}
+
+#[MyAttribute(1234), MyAttribute(5678)]
+class AnotherThing
+{
+}
