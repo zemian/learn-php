46528378d84c7ab3694c567951d4f3911facab66
Zemian Deng <zemiandeng@gmail.com>
2022-09-23 08:06:20 -0400

    Rename file to mysql specific

--- php-core/pdo/dbtest.php
+++ php-core/pdo/dbtest.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=information_schema', 'zemian', 'test123');
-$stmt = $db->query('SELECT VERSION()');
+$db = new PDO('sqlite:memory');
+$stmt = $db->query('SELECT 1 + 2');
 $mysql_version = $stmt->fetch()[0];
 echo $mysql_version;
