94295f4bf769b5029b1244e0e7a92405c1781848
Zemian Deng <zemiandeng@gmail.com>
2022-09-23 08:53:23 -0400

    Add db logging

---
+++ php-core/pdo/db-logging.php
@@ -0,0 +1,12 @@
+<?php
+// Use sqlite db to store logging data instead of logfile.
+$dbh = new PDO('sqlite:test.log.db');
+$result = $dbh->exec("CREATE TABLE IF NOT EXISTS logs(datetime, source, level, message)");
+
+$sth = $dbh->prepare("INSERT INTO logs VALUES(?, ?, ?, ?)");
+for ($i = 0; $i < 100; $i++) {
+    echo "Writing to log\n";
+    $sth->execute([date('c'), 'db-logging.php', 'debug', "Just a test #i"]);
+    sleep((rand(1, 3)));
+}
+echo "Done\n";
